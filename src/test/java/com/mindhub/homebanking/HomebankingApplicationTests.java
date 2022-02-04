cd990b7550cdc655aa23cecc099b18f6d368a77d
Alejandra Aguilera Azúa <73414955+alejandra-aa@users.noreply.github.com>
2022-02-04 18:12:30 -0300

    Update HomebankingApplicationTests.java

--- src/test/java/com/mindhub/homebanking/HomebankingApplicationTests.java
+++ src/test/java/com/mindhub/homebanking/HomebankingApplicationTests.java
@@ -3,6 +3,7 @@ package com.mindhub.homebanking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class HomebankingApplicationTests {
 
